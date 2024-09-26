0a542212acaf94e714dbaf7feb717affacb938dc
antonio.romanos@estudiantat.upc.edu <antonio.romanos@estudiantat.upc.edu>
2024-09-26 22:50:46 +0200

    Translations completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import robotsTxt from 'astro-robots-txt';
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://toto-rr.github.io',
-  base: '/ToniRomanos/',
+  i18n: {
+    defaultLocale: 'es',
+    locales: [ 'es', 'en', 'ca'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
