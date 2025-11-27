fb90e83fc1bd74f6b45a52ddd42105b91fa5690c
Toni Romanos <toto.romanos.ruiz@gmail.com>
2025-11-27 09:34:00 +0100

    Sitemap Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://toniromanos.dev',
+  site: 'https://www.toniromanos.dev',   // usa siempre el dominio principal con www si has elegido ese
   base: '/',
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap(),
+  ],
 });
